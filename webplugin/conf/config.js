a72ad4aa64465a25d4f393c4862e4b36e60cdab3
rudranand <k.anand2189@gmail.com>
2018-09-13 00:53:38 +0530

    handle config file path not found exception

--- webplugin/conf/config.js
+++ webplugin/conf/config.js
@@ -5,8 +5,10 @@
  */
 const externalConfig = process.argv.indexOf('--config') !== -1;
 const configPath = externalConfig ? process.argv[process.argv.indexOf('--config')+1]: __dirname+'/config.json';
-if (!require('fs').existsSync(configPath)) { 
-  console.error("\x1b[31m------------ config file not exist -----------------\x1b[0m")
+try {
+  require(configPath);
+} catch (err) {
+  console.log("\x1b[41m------ unable to find config file path:",configPath," -----\x1b[0m");
   process.exit(0);
 }
 const config = require(configPath);
